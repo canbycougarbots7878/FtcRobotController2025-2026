2ce7339e215a24774ff6c41b2d056a83c446330f
canbycougarbots7878 <cougarbots7878@gmail.com>
2026-01-14 16:55:42 -0800

    added library to control the arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -0,0 +1,95 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Arm {
+    private HardwareMap hardwareMap;
+
+    public DcMotor Arm_Motor;
+
+    Arm(HardwareMap hardwareMap){
+        // Arm initialization
+        this.Arm_Motor = hardwareMap.get(DcMotor.class, "arm");
+        this.Arm_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.Arm_Motor.setTargetPosition(0);
+        this.Arm_Motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        boolean armUp = false;
+    }
+
+    /**
+     *
+     * @param gamepad
+     * @param armUp  add "if (gamepad.xWasPressed() || gamepad.xWasPressed()) {
+     *             armUp = !armUp;
+     *               }" before this line of code (and set it at the start to false)
+     */
+
+    public void ArmMoveController(Gamepad gamepad, boolean armUp){
+        // Toggle arm position when X is pressed
+        if (gamepad.xWasPressed() || gamepad.xWasPressed()) {
+            this.Arm_Motor.setTargetPosition(armUp ? (gamepad.left_bumper ? 600 : 640) : 0);
+        }
+
+        int armPos = this.Arm_Motor.getCurrentPosition();
+
+        // Arm stopping and reset behavior
+        if (!(gamepad.x) && armPos < 30) {
+            this.Set_Arm_Power(0);
+            if (armPos < 10) {
+                this.Reset_Arm_Reading();
+            }
+        } else {
+            this.Set_Arm_Power(1);
+        }
+
+        if (gamepad.back) {
+            this.Reset_Arm_Reading();
+        }
+    }
+
+    // Arm functions
+    public void Set_Arm_Power(double power) {
+        this.Arm_Motor.setPower(power);
+    }
+
+    public void Set_Arm_Position(int tick) {
+        this.Arm_Motor.setTargetPosition(tick);
+    }
+
+    public int Get_Arm_Position() {
+        return this.Arm_Motor.getCurrentPosition();
+    }
+
+    public void Reset_Arm_Reading() {
+        Arm_Motor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        Arm_Motor.setTargetPosition(0);
+        Arm_Motor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+    }
+
+    public void Arm_Glide_To(int tick) {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
